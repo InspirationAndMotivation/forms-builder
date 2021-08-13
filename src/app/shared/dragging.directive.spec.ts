bfb121d2ce9308688cc547060a9be10be72f395c
Fie-fie <miss.pranovich@mail.ru>
2021-08-13 15:15:12 +0300

    Cleaned up some files

--- src/app/shared/dragging.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DraggingDirective } from './dragging.directive';
-
-describe('DraggingDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DraggingDirective();
-    expect(directive).toBeTruthy();
-  });
-});
